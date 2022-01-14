2ee686a8ff1de36ac53ff5075d76ed1d13114751
SBarn11 <sarabarnett1@outlook.com>
2022-01-13 18:13:28 -0700

    create secure .env file and link database

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const dbInfo =require('dotenv').config();
+const mysql = require('mysql2');
 
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to mysql database
+const db = mysql.createConnection(
+  {
+   host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  },
+  console.log('connected to the election database')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 
 
